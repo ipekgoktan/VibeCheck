119d5aeb7f2c7ddb267fcc70d8ad0c0c19a2ac7d
Ipek Goktan <ipekg@usc.edu>
2022-02-05 02:56:00 -0800

    image color api function

--- frontend/vibecheck/src/main.js
+++ frontend/vibecheck/src/main.js
@@ -4,13 +4,26 @@ const client = new vision.ImageAnnotatorClient({
     keyFilename: "../../../frontend/vibecheck/generated-surf-340405-f9295239e3f4.json",
 });
 
-client
-    .labelDetection("/Users/ipekgoktan/Documents/git/VibeCheck/frontend/vibecheck/src/picture.jpeg")
-    .then((results) => {
-        const labels = results[0].labelAnnotations;
-        console.log("Labels: ");
-        labels.forEach((label) => console.log(label.description));
-    })
-    .catch((err) => {
-        console.error("ERROR", err);
-    })
+const fileName = '../../../frontend/vibecheck/src/picture.jpeg';
+
+const detectLocalFile = async function() { //prints colors
+    const [result] = await client.imageProperties(fileName);
+    const colors = result.imagePropertiesAnnotation.dominantColors.colors;
+    colors.forEach(color => console.log(color));
+}
+
+detectLocalFile()
+
+
+// client
+//     .labelDetection("../../../frontend/vibecheck/src/picture.jpeg")
+//     .then((results) => {
+//         const labels = results[0].labelAnnotations;
+//         console.log("Labels: ");
+//         labels.forEach((label) => console.log(label.description));
+//     })
+//     .catch((err) => {
+//         console.error("ERROR", err);
+//     })
+
+
