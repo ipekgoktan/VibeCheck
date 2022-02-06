686ac4c1f96f9b3f16b0c30444cbd168da7c84cf
Ipek Goktan <ipekg@usc.edu>
2022-02-06 02:15:13 -0800

    gif

--- frontend/vibecheck/src/pages/Landing.js
+++ frontend/vibecheck/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import './Landing.css';
 import Prompt from './../images/LandingPics/Prompt.svg'
 import VibeCheck from './../images/LandingPics/VibeCheck.svg'
+import logo from './../images/LandingPics/vibecheck.gif'
 import Rectangle from './../images/LandingPics/Rectangle.svg'
 import button from './../images/LandingPics/button.svg'
 import {Link } from "react-router-dom";
@@ -10,7 +11,7 @@ function Landing() {
       <div className="Landing">
         <header className="Landing-header">
           <img src={Prompt} className="Landing-items" />
-          <img src={VibeCheck} className="Landing-items" />
+          <img src={logo} className="Landing-items" />
           <Link to={'/VibesPage'}>
             <img src={button} className="Landing-items" />
           </Link>
