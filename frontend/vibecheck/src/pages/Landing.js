92fe320262f7be7f329c06e4bb6dd247d1d405fe
Ipek Goktan <ipekg@usc.edu>
2022-02-05 16:58:17 -0800

    we can move between pages now!

---
+++ frontend/vibecheck/src/pages/Landing.js
@@ -0,0 +1,14 @@
+function Landing() {
+    return (
+      <div className="Landing">
+        <header className="Landing-header">
+          <p>
+            test!!
+          </p>
+        </header>
+      </div>
+    );
+}
+  
+  
+export default Landing;
